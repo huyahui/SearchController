4a785ea01320ff217dda1d24b76e7b0d7f550c44
胡亚辉 <695004098@qq.com>
2015-12-21 14:49:30 +0800

    搜索

---
+++ SearchController/MyTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  MyTableViewController.h
+//  SearchController
+//
+//  Created by 曦炽 朱 on 14/10/27.
+//  Copyright (c) 2014年 mirroon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+- (void)setName:(NSString *)name;
+- (NSString *)getName;
+@end
